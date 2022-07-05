6314a5e4806c92ba9d5b953d3ebdbe0c50c4c001
Renato Lui Geh <renatogeh@gmail.com>
2022-07-05 18:38:52 -0300

    poly: add brute-force solver for poly

--- pasp/poly.py
+++ pasp/poly.py
@@ -1,7 +1,7 @@
 import itertools, math
 
 import numpy as np
-from scipy.optimize import minimize
+from scipy.optimize import brute
 
 from .program import CredalFact
 
@@ -9,19 +9,49 @@ Product = list[bool]
 Polynomial = list[Product]
 Coefficients = list[float]
 
-def _obj_func(X: np.array, S: np.array, C: np.array):
-  X_cmpl = 1-X
-  return sum(np.prod(np.where(S[i], X, X_cmpl))*C[i] for i in range(len(C)))
+def _smp_obj_func(X: np.array, S: np.array, C: np.array) -> float:
+  n, X_cmpl = len(C), 1-X
+  return sum(np.prod(np.where(S[i], X, X_cmpl))*C[i] for i in range(n))
 
-def _obj_func_neg(X: np.array, S: np.array, C: np.array):
-  X_cmpl = 1-X
-  return -sum(np.prod(np.where(S[i], X, X_cmpl))*C[i] for i in range(len(C)))
+def _smp_obj_func_neg(X: np.array, S: np.array, C: np.array) -> float:
+  n, X_cmpl = len(C), 1-X
+  return -sum(np.prod(np.where(S[i], X, X_cmpl))*C[i] for i in range(n))
 
-def extrema(P: Polynomial, K: Coefficients, C: list[CredalFact], method: str = "Nelder-Mead") -> tuple[float, float]:
-  B = [(c.l, c.u) for c in C]
-  S, C = np.array(P), np.array(K)
-  l = minimize(_obj_func, x0 = np.fromiter(((l+u)/2 for l, u in B), dtype = float), \
-               args = (S, C), method = method, bounds = B)
-  u = minimize(_obj_func_neg, x0 = np.fromiter(((l+u)/2 for l, u in B), dtype = float), \
-               args = (S, C), method = method, bounds = B)
-  return (l.fun, -u.fun)
+def _obj_func(X: np.array, S: np.array, T: np.array, C: np.array, K: np.array) -> float:
+  n, m, X_cmpl = len(C), len(K), 1-X
+  x = 0 if len(S) == 0 else sum(np.prod(np.where(S[i], X, X_cmpl))*C[i] for i in range(n))
+  y = 0 if len(T) == 0 else sum(np.prod(np.where(T[i], X, X_cmpl))*K[i] for i in range(m))
+  return x/(x+y)
+
+def _obj_func_neg(X: np.array, S: np.array, T: np.array, C: np.array, K: np.array) -> float:
+  n, m, X_cmpl = len(C), len(K), 1-X
+  x = 0 if len(S) == 0 else sum(np.prod(np.where(S[i], X, X_cmpl))*C[i] for i in range(n))
+  y = 0 if len(T) == 0 else sum(np.prod(np.where(T[i], X, X_cmpl))*K[i] for i in range(m))
+  return -(x/(x+y))
+
+def minimize_smp(P: Polynomial, K: Coefficients, B: list[tuple[float, float]]) -> float:
+  return brute(_smp_obj_func, ranges = B, args = (np.array(P), np.array(K)), Ns = 2, finish = None, \
+               full_output = True)[1]
+
+def maximize_smp(P: Polynomial, K: Coefficients, B: list[tuple[float, float]]) -> float:
+  return -brute(_smp_obj_func_neg, ranges = B, args = (np.array(P), np.array(K)), Ns = 2, finish = None, \
+                full_output = True)[1]
+
+def minimize(P: Polynomial, Q: Polynomial, K: Coefficients, F: Coefficients, B: list[tuple[float, float]]) -> float:
+  return brute(_obj_func, ranges = B, args = (np.array(P), np.array(Q), np.array(K), np.array(F)), \
+               Ns = 2, finish = None, full_output = True)[1]
+
+def maximize(P: Polynomial, Q: Polynomial, K: Coefficients, F: Coefficients, B: list[tuple[float, float]]) -> float:
+  return -brute(_obj_func_neg, ranges = B, args = (np.array(P), np.array(Q), np.array(K), np.array(F)), \
+                Ns = 2, finish = None, full_output = True)[1]
+
+def extrema(P: Polynomial, Q: Polynomial, K: Coefficients, F: Coefficients, B: list[CredalFact]) -> tuple[float, float]:
+  S1, S2, K1, K2 = np.array(P), np.array(Q), np.array(K), np.array(F)
+  _, l, _, _ = brute(_obj_func, ranges = B, args = (S1, K1, S2, K2), Ns = 2, finish = None, \
+                     full_output = True)
+  _, u, _, _ = brute(_obj_func_neg, ranges = B, args = (S1, K1, S2, K2), Ns = 2, finish = None, \
+                     full_output = True)
+  return (l, -u)
+
+def print_poly(P: Polynomial, C: Coefficients):
+  print(" + ".join("·".join((f"{C[i]}", *(f"x_{j}" if P[i][j] else f"(1-x_{j})" for j in range(len(P[i]))))) for i in range(len(P))))
