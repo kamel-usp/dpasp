73658c9390fa83bf09871e26b33cf433d1dc8342
Renato Lui Geh <renatogeh@gmail.com>
2022-07-31 22:20:23 -0300

    tests: add C extension tests

--- tests/utils.py
+++ tests/utils.py
@@ -1,8 +1,10 @@
 import unittest
 
 class PaspTest(unittest.TestCase):
-  def assertApproxEqual(self, X: list, Y: list):
+  def assertApproxEqual(self, X: list, Y: list, Z: list = None):
     self.assertEqual(len(X), len(Y))
-    for x, y in zip(X, Y):
-      self.assertAlmostEqual(x, y)
+    if Z is not None: self.assertEqual(len(Y), len(Z))
+    for x, y in zip(X, Y): self.assertAlmostEqual(x, y)
+    if Z is not None:
+      for y, z in zip(Y, Z): self.assertAlmostEqual(y, z)
 
