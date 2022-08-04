e437edd72a3466aa4a08440d479cf3c96195590e
Renato Lui Geh <renatogeh@gmail.com>
2022-08-04 16:55:04 -0300

    carray: add char and clingo_symbol_t dynarrays

--- pasp/carray.c
+++ pasp/carray.c
@@ -2,6 +2,7 @@
 #include <Python.h>
 
 #include <stdarg.h>
+#include <string.h>
 
 #define CARRAY_MODULE
 #include "carray.h"
@@ -77,6 +78,65 @@ CARRAY_ARRAY_GROW_DECLARE(double);
 CARRAY_ARRAY_APPEND_DECLARE(double);
 /* CARRAY_ARRAY_EXTEND_NP_DECLARE(double); */
 
+CARRAY_ARRAY_INIT_DECLARE(clingo_symbol_t);
+CARRAY_ARRAY_FREE_CONTENTS_DECLARE(clingo_symbol_t);
+CARRAY_ARRAY_FREE_DECLARE(clingo_symbol_t);
+CARRAY_ARRAY_GROW_DECLARE(clingo_symbol_t);
+CARRAY_ARRAY_APPEND_DECLARE(clingo_symbol_t);
+
+/* Finds the next highest power of two integer.
+ * See https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2Float */
+#define NEXT_HIGHEST_2_POW_64(x) --x; x |= x >> 1; x |= x >> 2; x |= x >> 4; x |= x >> 8; x |= x >> 16; x |= x >> 32; ++x;
+#define NEXT_HIGHEST_2_POW(x) --x; x |= x >> 1; x |= x >> 2; x |= x >> 4; x |= x >> 8; x |= x >> 16; ++x;
+
+#undef ARRAY_MAGIC_MULTIPLIER
+#define ARRAY_MAGIC_MULTIPLIER 2
+CARRAY_ARRAY_INIT_DECLARE(char);
+CARRAY_ARRAY_FREE_CONTENTS_DECLARE(char);
+CARRAY_ARRAY_FREE_DECLARE(char);
+CARRAY_ARRAY_GROW_DECLARE(char);
+CARRAY_ARRAY_APPEND_DECLARE(char);
+static bool array_char_from(array_char_t *a, const char *s) {
+  size_t n = strlen(s)+1;
+  if (n > a->c) {
+    size_t c = n;
+    char *z;
+    NEXT_HIGHEST_2_POW(c);
+    z = (char*) realloc(a->d, c);
+    if (!z) {
+      a->d = NULL;
+      PyErr_SetString(PyExc_MemoryError, "could not allocate more memory for carray_string!");
+      return false;
+    }
+    a->d = z;
+    a->c = c;
+  }
+  memcpy(a->d, s, n);
+  a->n = n;
+  return true;
+}
+static bool array_char_writeln(array_char_t *a, char *s, size_t n) {
+  if ((a->n + n) > a->c) {
+    size_t c = a->n + n;
+    char *z;
+    NEXT_HIGHEST_2_POW(c);
+    z = (char*) realloc(a->d, c);
+    if (!z) {
+      a->d = NULL;
+      PyErr_SetString(PyExc_MemoryError, "could not allocate more memory for carray_string!");
+      return false;
+    }
+    a->d = z;
+    a->c = c;
+  }
+  if (a->n) a->d[a->n-1] = '\n';
+  memcpy(a->d + a->n, s, n);
+  a->n += n - 1;
+  return true;
+}
+#undef ARRAY_MAGIC_MULTIPLIER
+#define ARRAY_MAGIC_MULTIPLIER 1.5
+
 static PyMethodDef CarrayMethods[] = {
   {NULL, NULL, 0, NULL},
 };
@@ -105,6 +165,16 @@ PyMODINIT_FUNC PyInit_carray(void) {
   PyCarray_API[PyCarray_array_bool_free_contents_NUM] = (void*) array_bool_free_contents;
   PyCarray_API[PyCarray_array_bool_free_NUM] = (void*) array_bool_free;
   PyCarray_API[PyCarray_array_bool_append_NUM] = (void*) array_bool_append;
+  PyCarray_API[PyCarray_array_char_init_NUM] = (void*) array_char_init;
+  PyCarray_API[PyCarray_array_char_free_contents_NUM] = (void*) array_char_free_contents;
+  PyCarray_API[PyCarray_array_char_free_NUM] = (void*) array_char_free;
+  PyCarray_API[PyCarray_array_char_append_NUM] = (void*) array_char_append;
+  PyCarray_API[PyCarray_array_char_from_NUM] = (void*) array_char_from;
+  PyCarray_API[PyCarray_array_char_writeln_NUM] = (void*) array_char_writeln;
+  PyCarray_API[PyCarray_array_clingo_symbol_t_init_NUM] = (void*) array_clingo_symbol_t_init;
+  PyCarray_API[PyCarray_array_clingo_symbol_t_free_contents_NUM] = (void*) array_clingo_symbol_t_free_contents;
+  PyCarray_API[PyCarray_array_clingo_symbol_t_free_NUM] = (void*) array_clingo_symbol_t_free;
+  PyCarray_API[PyCarray_array_clingo_symbol_t_append_NUM] = (void*) array_clingo_symbol_t_append;
 
   c_api_object = PyCapsule_New((void*) PyCarray_API, "carray._C_API", NULL);
 
--- pasp/carray.h
+++ pasp/carray.h
@@ -7,6 +7,7 @@ extern "C" {
 
 #include <stdlib.h>
 #include <stdbool.h>
+#include <clingo.h>
 
 #define CARRAY_ARRAY_TYPE_DECLARE(type) \
   typedef struct array_##type { \
@@ -17,6 +18,8 @@ extern "C" {
 
 CARRAY_ARRAY_TYPE_DECLARE(bool);
 CARRAY_ARRAY_TYPE_DECLARE(double);
+CARRAY_ARRAY_TYPE_DECLARE(char);
+CARRAY_ARRAY_TYPE_DECLARE(clingo_symbol_t);
 
 #define PyCarray_array_double_init_NUM 0
 #define PyCarray_array_double_init_RETURN bool
@@ -50,7 +53,47 @@ CARRAY_ARRAY_TYPE_DECLARE(double);
 #define PyCarray_array_bool_append_RETURN bool
 #define PyCarray_array_bool_append_PROTO (array_bool_t *a, bool o)
 
-#define PyCarray_API_pointers 8
+#define PyCarray_array_char_init_NUM 8
+#define PyCarray_array_char_init_RETURN bool
+#define PyCarray_array_char_init_PROTO (array_char_t *a)
+
+#define PyCarray_array_char_free_contents_NUM 9
+#define PyCarray_array_char_free_contents_RETURN void
+#define PyCarray_array_char_free_contents_PROTO (array_char_t *a)
+
+#define PyCarray_array_char_free_NUM 10
+#define PyCarray_array_char_free_RETURN void
+#define PyCarray_array_char_free_PROTO (array_char_t *a)
+
+#define PyCarray_array_char_append_NUM 11
+#define PyCarray_array_char_append_RETURN bool
+#define PyCarray_array_char_append_PROTO (array_char_t *a, char o)
+
+#define PyCarray_array_char_from_NUM 12
+#define PyCarray_array_char_from_RETURN bool
+#define PyCarray_array_char_from_PROTO (array_char_t *a, const char *s)
+
+#define PyCarray_array_char_writeln_NUM 13
+#define PyCarray_array_char_writeln_RETURN bool
+#define PyCarray_array_char_writeln_PROTO (array_char_t *a, char *s, size_t n)
+
+#define PyCarray_array_clingo_symbol_t_init_NUM 14
+#define PyCarray_array_clingo_symbol_t_init_RETURN bool
+#define PyCarray_array_clingo_symbol_t_init_PROTO (array_clingo_symbol_t_t *a)
+
+#define PyCarray_array_clingo_symbol_t_free_contents_NUM 15
+#define PyCarray_array_clingo_symbol_t_free_contents_RETURN void
+#define PyCarray_array_clingo_symbol_t_free_contents_PROTO (array_clingo_symbol_t_t *a)
+
+#define PyCarray_array_clingo_symbol_t_free_NUM 16
+#define PyCarray_array_clingo_symbol_t_free_RETURN void
+#define PyCarray_array_clingo_symbol_t_free_PROTO (array_clingo_symbol_t_t *a)
+
+#define PyCarray_array_clingo_symbol_t_append_NUM 17
+#define PyCarray_array_clingo_symbol_t_append_RETURN bool
+#define PyCarray_array_clingo_symbol_t_append_PROTO (array_clingo_symbol_t_t *a, clingo_symbol_t o)
+
+#define PyCarray_API_pointers 18
 
 #ifdef CARRAY_MODULE
 
@@ -63,6 +106,18 @@ static PyCarray_array_double_init_RETURN array_double_init PyCarray_array_double
 static PyCarray_array_double_free_RETURN array_double_free PyCarray_array_double_free_PROTO;
 static PyCarray_array_double_free_contents_RETURN array_double_free_contents PyCarray_array_double_free_contents_PROTO;
 static PyCarray_array_double_append_RETURN array_double_append PyCarray_array_double_append_PROTO;
+
+static PyCarray_array_char_init_RETURN array_char_init PyCarray_array_char_init_PROTO;
+static PyCarray_array_char_free_RETURN array_char_free PyCarray_array_char_free_PROTO;
+static PyCarray_array_char_free_contents_RETURN array_char_free_contents PyCarray_array_char_free_contents_PROTO;
+static PyCarray_array_char_append_RETURN array_char_append PyCarray_array_char_append_PROTO;
+static PyCarray_array_char_from_RETURN array_char_from PyCarray_array_char_from_PROTO;
+static PyCarray_array_char_writeln_RETURN array_char_writeln PyCarray_array_char_writeln_PROTO;
+
+static PyCarray_array_clingo_symbol_t_init_RETURN array_clingo_symbol_t_init PyCarray_array_clingo_symbol_t_init_PROTO;
+static PyCarray_array_clingo_symbol_t_free_RETURN array_clingo_symbol_t_free PyCarray_array_clingo_symbol_t_free_PROTO;
+static PyCarray_array_clingo_symbol_t_free_contents_RETURN array_clingo_symbol_t_free_contents PyCarray_array_clingo_symbol_t_free_contents_PROTO;
+static PyCarray_array_clingo_symbol_t_append_RETURN array_clingo_symbol_t_append PyCarray_array_clingo_symbol_t_append_PROTO;
 
 #else
 
@@ -84,6 +139,26 @@ static void** PyCarray_API;
   (*(PyCarray_array_double_free_RETURN (*)PyCarray_array_double_free_PROTO) PyCarray_API[PyCarray_array_double_free_NUM])
 #define array_double_append \
   (*(PyCarray_array_double_append_RETURN (*)PyCarray_array_double_append_PROTO) PyCarray_API[PyCarray_array_double_append_NUM])
+#define array_char_init \
+  (*(PyCarray_array_char_init_RETURN (*)PyCarray_array_char_init_PROTO) PyCarray_API[PyCarray_array_char_init_NUM])
+#define array_char_free_contents \
+  (*(PyCarray_array_char_free_contents_RETURN (*)PyCarray_array_char_free_contents_PROTO) PyCarray_API[PyCarray_array_char_free_contents_NUM])
+#define array_char_free \
+  (*(PyCarray_array_char_free_RETURN (*)PyCarray_array_char_free_PROTO) PyCarray_API[PyCarray_array_char_free_NUM])
+#define array_char_append \
+  (*(PyCarray_array_char_append_RETURN (*)PyCarray_array_char_append_PROTO) PyCarray_API[PyCarray_array_char_append_NUM])
+#define array_char_from \
+  (*(PyCarray_array_char_from_RETURN (*)PyCarray_array_char_from_PROTO) PyCarray_API[PyCarray_array_char_from_NUM])
+#define array_char_writeln \
+  (*(PyCarray_array_char_writeln_RETURN (*)PyCarray_array_char_writeln_PROTO) PyCarray_API[PyCarray_array_char_writeln_NUM])
+#define array_clingo_symbol_t_init \
+  (*(PyCarray_array_clingo_symbol_t_init_RETURN (*)PyCarray_array_clingo_symbol_t_init_PROTO) PyCarray_API[PyCarray_array_clingo_symbol_t_init_NUM])
+#define array_clingo_symbol_t_free_contents \
+  (*(PyCarray_array_clingo_symbol_t_free_contents_RETURN (*)PyCarray_array_clingo_symbol_t_free_contents_PROTO) PyCarray_API[PyCarray_array_clingo_symbol_t_free_contents_NUM])
+#define array_clingo_symbol_t_free \
+  (*(PyCarray_array_clingo_symbol_t_free_RETURN (*)PyCarray_array_clingo_symbol_t_free_PROTO) PyCarray_API[PyCarray_array_clingo_symbol_t_free_NUM])
+#define array_clingo_symbol_t_append \
+  (*(PyCarray_array_clingo_symbol_t_append_RETURN (*)PyCarray_array_clingo_symbol_t_append_PROTO) PyCarray_API[PyCarray_array_clingo_symbol_t_append_NUM])
 
 static int import_carray(void) {
   PyCarray_API = (void**) PyCapsule_Import("carray._C_API", 0);
