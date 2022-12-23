1256d2ab759c3b8a5a8eb45eb5b4a13146b7aeb2
Renato Lui Geh <renatogeh@gmail.com>
2022-12-23 16:25:07 -0300

    setup: add learn module to build order

--- setup.py
+++ setup.py
@@ -17,27 +17,41 @@ class TestCommand(Command):
     os.system("python setup.py build_ext --inplace && python -m unittest tests/examples.py -b && " \
               "python -m unittest tests/counting.py -b")
 
+STD_MACROS = [("NUM_PROCS", str(os.cpu_count())), ("_GNU_SOURCE", None)]
+# Debug concurrency problems by forcing sequential running.
+# STD_MACROS = [("NUM_PROCS", str(1)), ("_GNU_SOURCE", None)]
+
 exact    = Extension("exact",
-                      libraries = ["m", "clingo", "pthread"],
-                      depends = ["pasp/cprogram.c", "pasp/coptimize.c", "pasp/cinf.c",
-                                 "pasp/cutils.c", "pasp/carray.c", "pasp/ground.c",
-                                 "pasp/cexact.c"],
-                      sources = ["pasp/exact.c", "thpool/thpool.c", "pasp/cinf.c",
-                                 "bitvector/bitvector.c", "pasp/cutils.c", "pasp/coptimize.c",
-                                 "pasp/carray.c", "pasp/cprogram.c", "pasp/cexact.c"],
-                      include_dirs = [np.get_include()],
-                      extra_compile_args = ["-Wno-unused-function"],
-                      define_macros = [("NUM_PROCS", str(os.cpu_count())), ("_GNU_SOURCE", None)])
+                     libraries = ["m", "clingo", "pthread"],
+                     depends = ["pasp/cprogram.c", "pasp/coptimize.c", "pasp/cinf.c",
+                                "pasp/cutils.c", "pasp/carray.c", "pasp/ground.c",
+                                "pasp/cexact.c"],
+                     sources = ["pasp/exact.c", "thpool/thpool.c", "pasp/cinf.c",
+                                "bitvector/bitvector.c", "pasp/cutils.c", "pasp/coptimize.c",
+                                "pasp/carray.c", "pasp/cprogram.c", "pasp/cexact.c"],
+                     include_dirs = [np.get_include()],
+                     extra_compile_args = ["-Wno-unused-function"],
+                     define_macros = STD_MACROS)
 ground   = Extension("ground",
-                      libraries = ["clingo"],
-                      depends = ["pasp/cutils.c", "pasp/cprogram.c", "pasp/ground.h",
-                                 "pasp/carray.c"],
-                      sources = ["pasp/ground.c", "pasp/cutils.c", "pasp/carray.c",
-                                 "pasp/cprogram.c"])
+                     libraries = ["clingo"],
+                     depends = ["pasp/cutils.c", "pasp/cprogram.c", "pasp/ground.h",
+                                "pasp/carray.c"],
+                     sources = ["pasp/ground.c", "pasp/cutils.c", "pasp/carray.c",
+                                "pasp/cprogram.c"])
+learn    = Extension("learn",
+                     libraries = ["clingo", "pthread"],
+                     depends = ["pasp/cprogram.c", "pasp/cinf.c", "pasp/cutils.c", "pasp/carray.c",
+                                "pasp.ground.c", "pasp/cexact.c", "pasp/clearn.c", "pasp/cdata.c"],
+                     sources = ["pasp/learn.c", "thpool/thpool.c", "pasp/cinf.c", "pasp/cprogram.c",
+                                "bitvector/bitvector.c", "pasp/cutils.c", "pasp/clearn.c",
+                                "pasp/carray.c", "pasp/cdata.c", "pasp/cexact.c", "pasp/coptimize.c"],
+                     include_dirs = [np.get_include()],
+                     extra_compile_args = ["-Wno-unused-function"],
+                     define_macros = STD_MACROS)
 
 setup(
   packages = find_packages(where = ".", include = ["pasp*"]),
   include_package_data = True,
-  ext_modules = [exact, ground],
+  ext_modules = [exact, ground, learn],
   cmdclass = {"test": TestCommand},
 )
