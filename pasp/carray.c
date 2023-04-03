14d9da9cfc5a6aaebf475fba239f2b19a890b2f3
Renato Lui Geh <renatogeh@gmail.com>
2023-04-03 17:01:48 -0300

    carray: macrofy array decl

--- pasp/carray.c
+++ pasp/carray.c
@@ -3,78 +3,15 @@
 
 #include "carray.h"
 
-#define ARRAY_MAGIC_MULTIPLIER 1.5
-#define ARRAY_MAGIC_INIT_CAP   16
-
-#define CARRAY_ARRAY_INIT_DECLARE(type) \
-bool array_##type##_init(array_##type##_t *a) { \
-  a->d = (type*) malloc(ARRAY_MAGIC_INIT_CAP*sizeof(type)); \
-  if (!a->d) { \
-    a->c = 0; \
-    return false; \
-  } \
-  a->c = ARRAY_MAGIC_INIT_CAP; \
-  a->n = 0; \
-  return true; \
-}
-
-#define CARRAY_ARRAY_FREE_CONTENTS_DECLARE(type) \
-void array_##type##_free_contents(array_##type##_t *a) { \
-  if (a->d) { free(a->d); a->d = NULL; a->c = 0; a->n = 0; } \
-}
-
-#define CARRAY_ARRAY_FREE_DECLARE(type) \
-void array_##type##_free(array_##type##_t *a) { array_##type##_free_contents(a); free(a); }
-
-#define CARRAY_ARRAY_GROW_DECLARE(type) \
-bool array_##type##_grow(array_##type##_t *a) { \
-  size_t nc = ARRAY_MAGIC_MULTIPLIER*a->c; \
-  size_t nb = sizeof(type)*nc; \
-  type *d = (type*) realloc(a->d, nb); \
-  if (!d) return false; \
-  a->d = d; \
-  a->c = nc; \
-  return true; \
-}
-
-#define CARRAY_ARRAY_APPEND_DECLARE(type) \
-bool array_##type##_append(array_##type##_t *a, type o) { \
-  if (a->n + 1 > a->c) { \
-    if (!array_##type##_grow(a)) return false; \
-    a->d[a->n++] = o; \
-  } else a->d[a->n++] = o; \
-  return true; \
-}
-
-#define CARRAY_ARRAY_EXTEND_DECLARE(type, promoted) \
-bool array_##type##_extend(array_##type##_t *a, size_t argc, ...) { \
-  va_list args; \
-  if ((a->n + argc > a->c)) if (!array_##type##_grow(a)) return false; \
-  va_start(args, argc); \
-  while (argc--) a->d[a->n++] = va_arg(args, promoted); \
-  return true; \
-}
 #define CARRAY_ARRAY_EXTEND_NP_DECLARE(type) CARRAY_ARRAY_EXTEND_DECLARE(type, type)
 
-CARRAY_ARRAY_INIT_DECLARE(bool)
-CARRAY_ARRAY_FREE_CONTENTS_DECLARE(bool)
-CARRAY_ARRAY_FREE_DECLARE(bool)
-CARRAY_ARRAY_GROW_DECLARE(bool)
-CARRAY_ARRAY_APPEND_DECLARE(bool)
+ARRAY_IMPL(bool)
 /* CARRAY_ARRAY_EXTEND_DECLARE(bool, int); */
 
-CARRAY_ARRAY_INIT_DECLARE(double)
-CARRAY_ARRAY_FREE_CONTENTS_DECLARE(double)
-CARRAY_ARRAY_FREE_DECLARE(double)
-CARRAY_ARRAY_GROW_DECLARE(double)
-CARRAY_ARRAY_APPEND_DECLARE(double)
+ARRAY_IMPL(double)
 /* CARRAY_ARRAY_EXTEND_NP_DECLARE(double); */
 
-CARRAY_ARRAY_INIT_DECLARE(clingo_symbol_t)
-CARRAY_ARRAY_FREE_CONTENTS_DECLARE(clingo_symbol_t)
-CARRAY_ARRAY_FREE_DECLARE(clingo_symbol_t)
-CARRAY_ARRAY_GROW_DECLARE(clingo_symbol_t)
-CARRAY_ARRAY_APPEND_DECLARE(clingo_symbol_t)
+ARRAY_IMPL(clingo_symbol_t)
 
 /* Finds the next highest power of two integer.
  * See https://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2Float */
@@ -83,11 +20,9 @@ CARRAY_ARRAY_APPEND_DECLARE(clingo_symbol_t)
 
 #undef ARRAY_MAGIC_MULTIPLIER
 #define ARRAY_MAGIC_MULTIPLIER 2
-CARRAY_ARRAY_INIT_DECLARE(char)
-CARRAY_ARRAY_FREE_CONTENTS_DECLARE(char)
-CARRAY_ARRAY_FREE_DECLARE(char)
-CARRAY_ARRAY_GROW_DECLARE(char)
-CARRAY_ARRAY_APPEND_DECLARE(char)
+
+ARRAY_IMPL(char)
+
 bool array_char_from(array_char_t *a, const char *s) {
   size_t n = strlen(s)+1;
   if (n > a->c) {
@@ -95,10 +30,7 @@ bool array_char_from(array_char_t *a, const char *s) {
     char *z;
     NEXT_HIGHEST_2_POW(c);
     z = (char*) realloc(a->d, c);
-    if (!z) {
-      a->d = NULL;
-      return false;
-    }
+    if (!z) return false;
     a->d = z;
     a->c = c;
   }
@@ -113,10 +45,7 @@ bool array_char_writeln(array_char_t *a, char *s, size_t n) {
     char *z;
     NEXT_HIGHEST_2_POW(c);
     z = (char*) realloc(a->d, c);
-    if (!z) {
-      a->d = NULL;
-      return false;
-    }
+    if (!z) return false;
     a->d = z;
     a->c = c;
   }
@@ -127,3 +56,5 @@ bool array_char_writeln(array_char_t *a, char *s, size_t n) {
 }
 #undef ARRAY_MAGIC_MULTIPLIER
 #define ARRAY_MAGIC_MULTIPLIER 1.5
+
+ARRAY_IMPL(uint8_t)
--- pasp/carray.h
+++ pasp/carray.h
@@ -5,6 +5,9 @@
 #include <stdbool.h>
 #include <clingo.h>
 
+#define ARRAY_MAGIC_MULTIPLIER 1.5
+#define ARRAY_MAGIC_INIT_CAP   16
+
 #define CARRAY_ARRAY_TYPE_DECLARE(type) \
   typedef struct array_##type { \
     type *d; /* data */ \
@@ -12,35 +15,109 @@
     size_t n; /* size */ \
   } array_##type##_t
 
-CARRAY_ARRAY_TYPE_DECLARE(bool);
-CARRAY_ARRAY_TYPE_DECLARE(double);
-CARRAY_ARRAY_TYPE_DECLARE(char);
-CARRAY_ARRAY_TYPE_DECLARE(clingo_symbol_t);
-
 #define CARRAY_ARRAY_HEADER(type, ret_type, func) \
 ret_type array_##type##_##func(array_##type##_t *a)
 #define CARRAY_ARRAY_HEADER_PONE(type, ret_type, func) \
 ret_type array_##type##_##func(array_##type##_t *a, type o)
+#define CARRAY_ARRAY_HEADER_ARG(type, ret_type, arg_type, func) \
+ret_type array_##type##_##func(array_##type##_t *a, arg_type o)
 
-CARRAY_ARRAY_HEADER(bool, bool, init);
-CARRAY_ARRAY_HEADER(bool, void, free_contents);
-CARRAY_ARRAY_HEADER(bool, void, free);
-CARRAY_ARRAY_HEADER_PONE(bool, bool, append);
+#define CARRAY_ARRAY_INIT_DECLARE(type) \
+bool array_##type##_init(array_##type##_t *a) { \
+  a->d = (type*) malloc(ARRAY_MAGIC_INIT_CAP*sizeof(type)); \
+  if (!a->d) { \
+    a->c = 0; \
+    return false; \
+  } \
+  a->c = ARRAY_MAGIC_INIT_CAP; \
+  a->n = 0; \
+  return true; \
+}
 
-CARRAY_ARRAY_HEADER(double, bool, init);
-CARRAY_ARRAY_HEADER(double, void, free_contents);
-CARRAY_ARRAY_HEADER(double, void, free);
-CARRAY_ARRAY_HEADER_PONE(double, bool, append);
+#define CARRAY_ARRAY_INITN_DECLARE(type) \
+bool array_##type##_initn(array_##type##_t *a, size_t c) { \
+  a->d = (type*) malloc(c*sizeof(type)); \
+  if (!a->d) { \
+    a->c = 0; \
+    return false; \
+  } \
+  a->c = c; \
+  a->n = 0; \
+  return true; \
+}
 
-CARRAY_ARRAY_HEADER(clingo_symbol_t, bool, init);
-CARRAY_ARRAY_HEADER(clingo_symbol_t, void, free_contents);
-CARRAY_ARRAY_HEADER(clingo_symbol_t, void, free);
-CARRAY_ARRAY_HEADER_PONE(clingo_symbol_t, bool, append);
+#define CARRAY_ARRAY_FREE_CONTENTS_DECLARE(type) \
+void array_##type##_free_contents(array_##type##_t *a) { \
+  if (a->d) { free(a->d); a->d = NULL; a->c = 0; a->n = 0; } \
+}
 
-CARRAY_ARRAY_HEADER(char, bool, init);
-CARRAY_ARRAY_HEADER(char, void, free_contents);
-CARRAY_ARRAY_HEADER(char, void, free);
-CARRAY_ARRAY_HEADER_PONE(char, bool, append);
+#define CARRAY_ARRAY_FREE_DECLARE(type) \
+void array_##type##_free(array_##type##_t *a) { array_##type##_free_contents(a); free(a); }
+
+#define CARRAY_ARRAY_GROW_DECLARE(type) \
+bool array_##type##_grow(array_##type##_t *a) { \
+  size_t nc = ARRAY_MAGIC_MULTIPLIER*a->c; \
+  size_t nb = sizeof(type)*nc; \
+  type *d = (type*) realloc(a->d, nb); \
+  if (!d) return false; \
+  a->d = d; \
+  a->c = nc; \
+  return true; \
+}
+
+#define CARRAY_ARRAY_APPEND_DECLARE(type) \
+bool array_##type##_append(array_##type##_t *a, type o) { \
+  if (a->n + 1 > a->c) { \
+    if (!array_##type##_grow(a)) return false; \
+    a->d[a->n++] = o; \
+  } else a->d[a->n++] = o; \
+  return true; \
+}
+
+#define CARRAY_ARRAY_EXTEND_DECLARE(type, promoted) \
+bool array_##type##_extend(array_##type##_t *a, size_t argc, ...) { \
+  va_list args; \
+  if ((a->n + argc > a->c)) if (!array_##type##_grow(a)) return false; \
+  va_start(args, argc); \
+  while (argc--) a->d[a->n++] = va_arg(args, promoted); \
+  return true; \
+}
+
+#define CARRAY_ARRAY_CLEAR_DECLARE(type) void array_##type##_clear(array_##type##_t *a) { a->n = 0; }
+
+#define ARRAY_IMPL(t) \
+  CARRAY_ARRAY_INIT_DECLARE(t) \
+  CARRAY_ARRAY_INITN_DECLARE(t) \
+  CARRAY_ARRAY_FREE_CONTENTS_DECLARE(t) \
+  CARRAY_ARRAY_FREE_DECLARE(t) \
+  CARRAY_ARRAY_GROW_DECLARE(t) \
+  CARRAY_ARRAY_APPEND_DECLARE(t) \
+  CARRAY_ARRAY_CLEAR_DECLARE(t)
+
+#define ARRAY_DECL(t) \
+  CARRAY_ARRAY_TYPE_DECLARE(t); \
+  CARRAY_ARRAY_HEADER(t, bool, init); \
+  CARRAY_ARRAY_HEADER(t, void, free_contents); \
+  CARRAY_ARRAY_HEADER(t, void, free); \
+  CARRAY_ARRAY_HEADER_PONE(t, bool, append); \
+  CARRAY_ARRAY_HEADER_ARG(t, bool, size_t, initn); \
+  CARRAY_ARRAY_HEADER(t, void, clear);
+
+ARRAY_DECL(bool)
+typedef array__Bool_t array_bool_t;
+#define array_bool_init array__Bool_init
+#define array_bool_free_contents array__Bool_free_contents
+#define array_bool_free array__Bool_free
+#define array_bool_append array__Bool_append
+#define array_bool_initn array__Bool_initn
+#define array_bool_clear array__Bool_clear
+
+ARRAY_DECL(double)
+ARRAY_DECL(char)
+ARRAY_DECL(clingo_symbol_t)
+typedef array_clingo_symbol_t_t array_symbol_t;
+ARRAY_DECL(uint8_t)
+typedef array_uint8_t_t array_uint8_t;
 
 bool array_char_from(array_char_t *a, const char *s);
 bool array_char_writeln(array_char_t *a, char *s, size_t n);
