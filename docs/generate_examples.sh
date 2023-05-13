3fe4897cb0aab6b127d3eb4a0f456b37fa73e9a3
Renato Lui Geh <renatogeh@gmail.com>
2023-05-13 15:30:18 -0300

    docs: actions: revert back docs to root

--- docs/generate_examples.sh
+++ docs/generate_examples.sh
@@ -2,6 +2,6 @@
 
 echo "Generating HTML from examples..."
 for f in examples/*; do
-  pygmentize -f html -O style=pastie,linenos=1,full,wrapcode -l Pasp "$f" > "_site/docs/${f%.*}.html"
+  pygmentize -f html -O style=pastie,linenos=1,full,wrapcode -l Pasp "$f" > "_site/${f%.*}.html"
 done
 echo "Done!"
