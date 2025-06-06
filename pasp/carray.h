649eae1fea9b1146810f84f8ae29140c0863d523
Renato Lui Geh <renatogeh@gmail.com>
2025-06-06 13:35:11 -0700

    carray: add uint64 dynamic array declaration

--- pasp/carray.h
+++ pasp/carray.h
@@ -118,6 +118,8 @@ ARRAY_DECL(clingo_symbol_t)
 typedef array_clingo_symbol_t_t array_symbol_t;
 ARRAY_DECL(uint8_t)
 typedef array_uint8_t_t array_uint8_t;
+ARRAY_DECL(uint64_t)
+typedef array_uint64_t_t array_uint64_t;
 
 bool array_char_from(array_char_t *a, const char *s);
 bool array_char_writeln(array_char_t *a, char *s, size_t n);
